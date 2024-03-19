b594814d1d6099b057b348bc8416bbf4e86cb30b
MuhammadqodirProgrammer <159247201+komilov02@users.noreply.github.com>
2024-03-19 10:07:50 +0500

    hello

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app =express()
 require('dotenv').config()
 const cors =require("cors")
 
-const routes =require('./routers')
+const routes =require('./src/routers')
 const PORT =process.env.PORT
 app.use(express.json())
 app.use(cors())
