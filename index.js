8824dd2d01d046db33631455cfca6c552519917a
MuhammadqodirProgrammer <159247201+komilov02@users.noreply.github.com>
2024-03-19 10:09:41 +0500

    hello

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express =require("express")
-const app =express()
-require('dotenv').config()
-const cors =require("cors")
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
 
-const routes =require('./src/routers')
-const PORT =process.env.PORT
-app.use(express.json())
-app.use(cors())
+const routes = require('./src/routers');
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cors());
 
-app.use(routes)
+app.use(routes);
+app.get('/', async (req, res) => {
+	try {
+		res.status(200).json({ status: 'OK', data: 'hello world' });
+	} catch (error) {
+		res.status(500).json({ messege: 'Internal server error', error });
+	}
+});
 
-app.listen(PORT ,()=>{
-    console.log(`SERVER RUNNIG ON PORT: ${PORT} `);
-})
+app.listen(PORT, () => {
+	console.log(`SERVER RUNNIG ON PORT: ${PORT} `);
+});
